3e29d9f3cc0cf8d66b4c39624561357400190f5b
MOHAMMED SHOEB ALI <150115059+shoeb-msa@users.noreply.github.com>
2026-07-09 07:38:41 +0530

    Time: 1 ms (98.56%) | Memory: 47.8 MB (25.45%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,29 +2,16 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] arr = new int[n][m];
-        for(int[] i:arr)
-        {
-        Arrays.fill(i,-1);
-        }
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
-
-                    for(int k=0;k<n;k++)
-                    {
-                        arr[k][j] = 0;
-                    }
-
-
-                    for(int k=0;k<m;k++)
-                    {
-                        arr[i][k] = 0;
-
-                    }
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
@@ -32,9 +19,9 @@ class Solution {
         {
             for(int j=0;j<m;j++)
             {
-                if(arr[i][j]== 0)
+                if(row[i] || col[j])
                 {
-                matrix[i][j] = arr[i][j];
+                matrix[i][j] = 0;
                 }
                
             }
